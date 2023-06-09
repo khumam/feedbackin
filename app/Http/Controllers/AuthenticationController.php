1378f83a89b6d6cfb604ccf6da27c67fef34bb22
Khoerul Umam <khoerul.umam@gotoko.co.id>
2023-06-09 10:24:12 +0700

    init homepage

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class AuthenticationController extends Controller
+{
+    /**
+     * googleAuth redirect
+     *
+     * @return void
+     */
+    public function googleAuth()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    /**
+     * googleAuthCallback
+     *
+     * @return void
+     */
+    public function googleAuthCallback()
+    {
+        $response = Socialite::driver('google')->user();
+        $user = User::updateOrCreate([
+            'provider' => 'google',
+            'provider_id' => $response->id
+        ], [
+            'name' => $response->name,
+            'email' => $response->email,
+            'avatar' => $response->avatar,
+            'provider_token' => $response->token,
+            'provider_refresh_token' => $response->refreshToken,
+            'provider_expires_in' => $response->expiresIn,
+            'password' => $response->id
+        ]);
+        Auth::login($user, true);
+        return redirect('/dashboard');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/auth/google', [AuthenticationController::class, 'googleAuth'])->name('auth.google');
+Route::get('/auth/google/callback', [AuthenticationController::class, 'googleAuthCallback'])->name('auth.google.callback');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('/project', ProjectController::class);
+});
+
+require __DIR__.'/auth.php';
